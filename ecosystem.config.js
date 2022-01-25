983c399c4b4858e84c1dff6ab5b3d46e902e55e1
Geraldo Amorim <gamorim@bluelab.com.br>
2022-01-25 16:02:35 -0300

    testing env and pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
     deploy : {
         app : {
           user : "root",
-          key  : "deploy.key",
+          key  : "key",
           host : "137.184.79.60",
           ref  : process.env.GITHUB_REF_NAME,
           repo : "git@github.com:" + process.env.GITHUB_REPOSITORY + ".git",
