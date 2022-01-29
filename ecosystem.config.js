ac5dff37ff30051aec1cd031412f87ca0c88879d
Geraldo Amorim <gamorim@bluelab.com.br>
2022-01-29 17:33:22 -0300

    testing env and pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
           key  : "key",
           host : "137.184.79.60",
           ssh_options: "StrictHostKeyChecking=no",
-          ref  : process.env.GITHUB_REF_NAME,
+          ref  : "master",
           repo : "git@github.com:" + process.env.GITHUB_REPOSITORY + ".git",
           path : "/root/app",
           "post-deploy" : "yarn install && pm2 startOrRestart ecosystem.config.js --env production"
